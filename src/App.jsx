01ba1aa5239d9241628498832bd0015ea3303f57
Rodrigo Galvão <rodrigogalvao136@gmail.com>
2024-04-18 20:57:51 -0300

    Update: Criando conteúdo da página e estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
-     hello, world!
+
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+
     </>
   )
 }
